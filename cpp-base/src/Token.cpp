a95fa79e81b4449605fed92efb9b285cb7aa3bfb
Alexander Wilson <acwilson96@gmail.com>
2018-10-27 12:24:45 +0100

    Token Copy/Assign operators.

--- cpp-base/src/Token.cpp
+++ cpp-base/src/Token.cpp
@@ -1,6 +1,17 @@
 #include "../include/Token.h"
 #include "../include/Position.h"
 
+ACC::Token::Token()
+    : tokenClass(ACC::Token::TokenClass::INVALID), position(Position(-1, -1)),
+      data("") {}
+
 ACC::Token::Token(ACC::Token::TokenClass type, int lineNum, int colNum,
                   std::string data)
     : tokenClass(type), position(Position(lineNum, colNum)), data(data) {}
+
+ACC::Token::Token(const ACC::Token &t)
+    : tokenClass(t.tokenClass), position(t.position), data(t.data) {}
+
+ACC::Token ACC::Token::operator=(Token t) {
+  return t;
+}
