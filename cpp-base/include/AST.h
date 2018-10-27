acfbc6b61e428dbf9ba79d62b5959b5227f0b576
Alexander Wilson <acwilson96@gmail.com>
2018-10-27 16:09:23 +0100

    Export more AST nodes.

--- cpp-base/include/AST.h
+++ cpp-base/include/AST.h
@@ -1,6 +1,9 @@
 #pragma once
 
+#include "ast/BaseType.h"
 #include "ast/FunDecl.h"
+#include "ast/PrimitiveType.h"
 #include "ast/Program.h"
 #include "ast/StructTypeDecl.h"
+#include "ast/Type.h"
 #include "ast/VarDecl.h"
