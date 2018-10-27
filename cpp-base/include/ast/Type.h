d2accb8e48fe3957ca72fd1aed7c84bb43874b6d
Alexander Wilson <acwilson96@gmail.com>
2018-10-27 16:08:14 +0100

    Implement toString() and instanceOf() to allow matching of Type's derivatives.

--- cpp-base/include/ast/Type.h
+++ cpp-base/include/ast/Type.h
@@ -1,11 +1,20 @@
 #pragma once
 
+#include <string>
+
 namespace ACC {
 
 class Type {
 
 public:
-  Type() = delete;
+  Type() {}
+
+  virtual std::string toString() const { return "Type::T"; }
 };
+
+template <typename Parent, typename Child>
+static bool instanceOf(const Child &child) {
+  return (typeid(Parent) == typeid(child));
+}
 
 }; // namespace ACC
