a430dbb661ad2286cc8a232c97c424ac8b619475
Alexander Wilson <acwilson96@gmail.com>
2018-11-10 18:01:24 +0000

    Scanner Throws when Attempting to Fetch beyond the EOF.

--- cpp-base/src/Scanner.cpp
+++ cpp-base/src/Scanner.cpp
@@ -4,8 +4,8 @@
 
 using namespace ACC;
 
-Scanner::Scanner(const std::string &filename) : column(1), line(1) {
-  std::ifstream t(filename);
+Scanner::Scanner(const std::string &filename)
+    : column(1), line(1)
   file = std::string((std::istreambuf_iterator<char>(t)),
                      std::istreambuf_iterator<char>());
   currChar = file.begin();
@@ -13,6 +13,9 @@ Scanner::Scanner(const std::string &filename) : column(1), line(1) {
 
 char Scanner::next() {
   char nextChar = *currChar;
+  if ((currChar - 1) == file.end())
+    throw std::runtime_error(
+        "Internal Error: Attempted to fetch character past EOF.");
   if (nextChar == '\n' || nextChar == '\r') {
     line++;
     column = 0;
@@ -23,4 +26,9 @@ char Scanner::next() {
   return nextChar;
 }
 
-char Scanner::peek() { return *currChar; }
+char Scanner::peek() {
+  if ((currChar - 1) == file.end())
+    throw std::runtime_error(
+        "Internal Error: Attempted to peek character past EOF.");
+  return *currChar;
+}
