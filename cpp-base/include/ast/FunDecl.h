7cc52f176d681e3cad0a848ea7542567ef0cf5db
Alexander Wilson <acwilson96@gmail.com>
2018-11-19 15:36:05 +0000

    FunDecl takes ownership for its attributes.

--- cpp-base/include/ast/FunDecl.h
+++ cpp-base/include/ast/FunDecl.h
@@ -14,13 +14,45 @@ namespace ACC {
 
 class FunDecl : public Decl {
 
+  std::shared_ptr<Block> funBlock;
+  std::string funName;
+  std::vector<std::shared_ptr<VarDecl>> funParams;
+  std::shared_ptr<Type> funType;
+
 public:
-  FunDecl(std::shared_ptr<Type> funType, std::string funName,
-          std::vector<std::shared_ptr<VarDecl>> funArgs, Block funBlock) {
+  FunDecl(std::shared_ptr<Block> funBlock, std::string funName,
+          std::vector<std::shared_ptr<VarDecl>> funParams,
+          std::shared_ptr<Type> funType)
+      : funBlock(funBlock), funName(funName), funParams(funParams),
+        funType(funType) {
     // Boilerplate Code.
   }
 
-  bool operator==(const FunDecl &fd) const { return true; }
+  bool operator==(const FunDecl &fd) const {
+    /* Check Block */
+    // TODO
+
+    /* Check Name */
+    if (fd.funName != funName)
+      return false;
+
+    /* Check Params */
+    if (fd.funParams.size() != funParams.size())
+      return false;
+    for (int i=0; i < funParams.size(); i++) {
+      VarDecl lhsParam = *funParams[i];
+      VarDecl rhsParam = *fd.funParams[i];
+
+      if (lhsParam != rhsParam)
+        return false;
+    }
+
+    /* Check Type */
+    if (*funType != *fd.funType)
+      return false;
+
+    return true;
+  }
 
   std::string toString() const override { return "FunDecl"; }
 };
