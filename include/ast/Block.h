ee136649bf0fb3d798f7a5197bcfaf44f873486d
Alexander Wilson <acwilson96@gmail.com>
2018-11-25 02:32:37 +0000

    Block's track their parent blocks, and their declarations.

--- include/ast/Block.h
+++ include/ast/Block.h
@@ -3,6 +3,10 @@
 #ifndef BLOCK_H
 #define BLOCK_H
 
+#include <map>
+#include <vector>
+
+#include "Decl.h"
 #include "Stmt.h"
 
 namespace ACC {
@@ -10,10 +14,35 @@ namespace ACC {
 class Block : public Stmt {
 
 public:
+  std::map<std::string, std::shared_ptr<Decl>> blockDecls;
   std::vector<std::shared_ptr<Stmt>> blockStmts;
+  std::shared_ptr<Block> outerBlock = nullptr;
 
-  Block(std::vector<std::shared_ptr<Stmt>> blockStmts)
-      : blockStmts(blockStmts) {}
+  Block(const std::vector<std::shared_ptr<Stmt>> &newBlockStmts)
+      : blockStmts(newBlockStmts) {}
+
+  std::shared_ptr<Decl> find(const std::string &identifier) {
+    std::shared_ptr<Decl> local = findCurrent(identifier);
+    if (local == nullptr && outerBlock != nullptr)
+      return outerBlock->find(identifier);
+
+    return nullptr;
+  }
+
+  std::shared_ptr<Decl> findCurrent(const std::string &identifier) {
+    if (blockDecls.find(identifier) != blockDecls.end())
+      return blockDecls.find(identifier)->second;
+
+    return nullptr;
+  }
+
+  void insertDecl(const std::shared_ptr<Decl> &decl) {
+    blockDecls[decl->getIdentifier()] = decl;
+  }
+
+  void setOuterBlock(const std::shared_ptr<Block> &newOuterBlock) {
+    outerBlock = newOuterBlock;
+  }
 
   std::string accept(ASTVisitor &v) override { return v.visit(*this); }
   std::string astClass() const override { return "Block"; }
