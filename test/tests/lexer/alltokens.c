7325e54d8b16c37e50c9b1ca9d874eddf3c7d13a
Alexander Wilson <acwilson96@gmail.com>
2018-11-29 01:40:55 +0000

    Test fixing AllTokens

--- test/tests/lexer/alltokens.c
+++ test/tests/lexer/alltokens.c
@@ -1,1 +1,1 @@
-iDenti0fier_0teSt9={}()[];,int void char if else while return struct sizeof #include include"foo bar baz""foo bar \n baz"0 01 012 0123 01234 012345 012356 01234567 012345678 0123456789 01234567890'a' 'b' 'A' '\t' '\t' '\b' '\n' '\r' '\f' '\'' '\"' '\\' '.' ',' '_'&&||==!=<><=>=+-*/%.
+iDenti0fier_0teSt9={}()[];,int void char if else while return struct sizeof include"foo bar baz""foo bar \n baz"0 01 012 0123 01234 012345 012356 01234567 012345678 0123456789 01234567890'a' 'b' 'A' '\t' '\t' '\b' '\n' '\r' '\f' '\'' '\"' '\\' '.' ',' '_'&&||==!=<><=>=+-*/%.
