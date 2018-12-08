aa67d5e3e3d469f49ab251e2038630e852b5f2ce
Alexander Wilson <acwilson96@gmail.com>
2018-12-08 12:45:55 +0000

    ASTNodes can exist in a file, line, column

--- include/ASTNode.h
+++ include/ASTNode.h
@@ -13,6 +13,10 @@ class Type;
 
 class ASTNode {
 public:
+  std::string file;
+  std::string line;
+  std::string column;
+  
   virtual void accept(ASTVisitor<void> &v) = 0;
   virtual std::string accept(ASTVisitor<std::string> &v) = 0;
   virtual std::shared_ptr<Type> accept(ASTVisitor<std::shared_ptr<Type>> &v) = 0;
