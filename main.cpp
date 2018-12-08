ba7b3cf370890a161d8d20dde577aba235f821d8
Alexander Wilson <acwilson96@gmail.com>
2018-12-08 21:17:13 +0000

    Support generation of MIPS.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 
 #include "include/ASTPasses.h"
+#include "include/CodeGeneration.h"
 #include "include/Lexer.h"
 #include "include/Parser.h"
 #include "include/Scanner.h"
@@ -10,6 +11,7 @@
 
 int main(int argc, char const *argv[]) {
   bool outputGraph = false;
+  bool outputMIPS = false;
   if (argc < 2) {
     std::cout << "Provide Filename" << std::endl;
     return 1;
@@ -18,8 +20,9 @@ int main(int argc, char const *argv[]) {
     std::string arg2(argv[2]);
     if (arg2 == "-p" || arg2 == "--print")
       outputGraph = true;
+    if (arg2 == "--mips")
+      outputMIPS = true;
   }
-
 
   std::string abspath(argv[1]);
 
@@ -38,13 +41,21 @@ int main(int argc, char const *argv[]) {
 
   ACC::NameAnalysis nameAnalysis(progAST);
   nameAnalysis.run();
-  if (nameAnalysis.errorCount > 0)
+  if (nameAnalysis.errorCount > 0) {
     nameAnalysis.printErrors();
-  else {
-    ACC::TypeAnalysis typeAnalysis(progAST);
-    typeAnalysis.run();
-    if (typeAnalysis.errorCount > 0)
-      typeAnalysis.printErrors();
+    return 1;
+  }
+
+  ACC::TypeAnalysis typeAnalysis(progAST);
+  typeAnalysis.run();
+  if (typeAnalysis.errorCount > 0) {
+    typeAnalysis.printErrors();
+    return 1;
+  }
+
+  if (outputMIPS) {
+    ACC::GenerateMIPS mipsGenerator(progAST, "mips.asm");
+    mipsGenerator.run();
   }
 
   return 0;
