6d80c827208a0fb06f7c4dd61474ebb4be390e60
Alexander Wilson <acwilson96@gmail.com>
2019-01-06 19:25:58 +0000

    Update preprocessors usage as standalone.

--- preprocessor/preprocessor.py
+++ preprocessor/preprocessor.py
@@ -1,3 +1,4 @@
+import sys
 from subprocess import call
 
 
@@ -6,11 +7,19 @@ class Preprocessor:
     # Calls the clang++ preprocessor on an input,
     # and writes the output to an output
     def preprocess(self, inFile, outFile):
-        # clang++ -std=c++14 -E compiler/main.cpp -o tempSource.cpp
-        call(["clang++", "-std=c++14", "-E",
-              inFile, "-o", outFile])
-        return outFile
+        if ".cpp" in inFile:
+            # clang++ -std=c++14 -E compiler/main.cpp -o tempSource.cpp
+            call(["clang++", "-std=c++14", "-E", inFile, "-o", outFile])
+        else:
+            # clang++ -std=c++14 -E compiler/main.cpp -o tempSource.cpp
+            call(["clang", "-E", inFile, "-o", outFile])
 
 
 if __name__ == "__main__":
-    print("Preprocessor cannot be used as a standalone script.")
+    print("Preprocessor should not be used as a standalone script.")
+    inFile = sys.argv[1]
+    outFile = sys.argv[2]
+    print("In: " + inFile)
+    print("Out: " + outFile)
+    pp = Preprocessor()
+    pp.preprocess(inFile, outFile)
