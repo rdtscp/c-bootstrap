43c6091c5154a7f95e734f0be86a89090082aa36
Alexander Wilson <acwilson96@gmail.com>
2019-03-02 16:55:00 +0000

    Decls now include their Visibility.

--- compiler/include/ast/Decl.h
+++ compiler/include/ast/Decl.h
@@ -9,6 +9,12 @@ namespace ACC {
 
 class Decl : public ASTNode {
 public:
+  enum class Visibility { PUBLIC, PRIVATE, PROTECTED, NONE };
+
+  Visibility visibility;
+
+  Decl() : visibility(Visibility::NONE) {}
+  Decl(const Visibility &visibility) : visibility(visibility) {}
   virtual ~Decl() {}
   virtual bool operator==(Decl &rhs) const = 0;
   virtual bool operator!=(Decl &rhs) const = 0;
