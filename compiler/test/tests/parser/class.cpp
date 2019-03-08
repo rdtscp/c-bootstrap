aa84ec40e389646c32d8b1b13aaf679adc699381
Alexander Wilson <acwilson96@gmail.com>
2019-03-08 00:22:03 +0000

    Config for debugging the parser/class.cpp file.

--- compiler/test/tests/parser/class.cpp
+++ compiler/test/tests/parser/class.cpp
@@ -1,11 +1,18 @@
 class className {
 
+private:
+  int privateInt;
+  const int constInt;
+
 public:
   className() {}
+  className(int a, char b) {}
 };
 
 int main(int argc, char *argv) {
   className foo;
   className bar;
+  className *baz = new className(1, 'b');
+  int *intarr = new int[5];
   return 1;
 }
