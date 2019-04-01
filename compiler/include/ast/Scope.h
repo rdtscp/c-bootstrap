cb0c249dcf8a7287c81439b5c559615c3f09e082
Alexander Wilson <acwilson96@gmail.com>
2019-04-01 21:51:18 +0100

    Move Scope implementation into a cpp file.

--- compiler/include/ast/Scope.h
+++ compiler/include/ast/Scope.h
@@ -3,35 +3,28 @@
 #include "atl/include/shared_ptr.h"
 #include "atl/include/vector.h"
 
+#include "Decl.h"
+
 namespace ACC {
-class Decl;
 
 class Scope {
 public:
   atl::vector<atl::shared_ptr<Decl>> decls;
   atl::shared_ptr<Scope> outerScope;
 
-  Scope() : decls({}), outerScope(nullptr) {}
+  Scope();
   ~Scope() {}
 
-  void insertDecl(const atl::shared_ptr<Decl> &decl) { decls.push_back(decl); }
-  atl::shared_ptr<Decl> find(const atl::shared_ptr<Identifier> &identifier) {
-    atl::shared_ptr<Decl> local = findLocal(identifier);
-    if (local == nullptr && outerScope != nullptr)
-      return outerScope->find(identifier);
+  void insertDecl(const atl::shared_ptr<Decl> &decl);
 
-    return local;
-  }
   atl::shared_ptr<Decl>
-  findLocal(const atl::shared_ptr<Identifier> &identifier) {
-    // Reverse iterate decls.
-    for (int idx = decls.size() - 1; idx >= 0; --idx) {
-      const atl::shared_ptr<Decl> &currDecl = decls[idx];
-      if (*identifier == *currDecl->getIdentifier())
-        return decls[idx];
-    }
+  resolveVarExpr(const atl::shared_ptr<Identifier> identifier) const;
+  atl::shared_ptr<Decl> resolveFunCall(const atl::string funSignature) const;
 
-    return nullptr;
-  }
+  atl::shared_ptr<Decl>
+  duplicateDeclaration(const atl::shared_ptr<Decl> &decl) const;
+
+  atl::shared_ptr<Decl>
+  duplicateDeclarationLocal(const atl::shared_ptr<Decl> &decl) const;
 };
 } // namespace ACC
---
+++ compiler/src/ast/Scope.cpp
@@ -0,0 +1,62 @@
+#include "../../include/ast/Scope.h"
+#include "../../include/ast/FunDecl.h"
+
+using namespace ACC;
+
+Scope::Scope() : decls({}), outerScope(nullptr) {}
+
+void Scope::insertDecl(const atl::shared_ptr<Decl> &decl) {
+  decls.push_back(decl);
+}
+
+atl::shared_ptr<Decl>
+Scope::resolveVarExpr(const atl::shared_ptr<Identifier> identifier) const {
+  for (int idx = decls.size() - 1; idx >= 0; --idx) {
+    if (decls[idx]->astClass() == "VarDecl" ||
+        decls[idx]->astClass() == "VarDef") {
+      if (*decls[idx]->getIdentifier() == *identifier)
+        return decls[idx];
+    }
+  }
+  if (outerScope != nullptr)
+    return outerScope->resolveVarExpr(identifier);
+
+  return nullptr;
+}
+
+atl::shared_ptr<Decl>
+Scope::resolveFunCall(const atl::string funSignature) const {
+  for (int idx = decls.size() - 1; idx >= 0; --idx) {
+    if (decls[idx]->astClass() == "FunDecl" ||
+        decls[idx]->astClass() == "FunDef") {
+      const atl::shared_ptr<FunDecl> currFunDecl =
+          atl::static_pointer_cast<FunDecl>(decls[idx]);
+      if (currFunDecl->getSignature() == funSignature)
+        return currFunDecl;
+    }
+  }
+  if (outerScope != nullptr)
+    return outerScope->resolveFunCall(funSignature);
+
+  return nullptr;
+}
+
+atl::shared_ptr<Decl>
+Scope::duplicateDeclaration(const atl::shared_ptr<Decl> &decl) const {
+  const atl::shared_ptr<Decl> &local = duplicateDeclarationLocal(decl);
+  if (local == nullptr && outerScope != nullptr)
+    return outerScope->duplicateDeclaration(decl);
+
+  return local;
+}
+
+atl::shared_ptr<Decl>
+Scope::duplicateDeclarationLocal(const atl::shared_ptr<Decl> &decl) const {
+  // Reverse iterate decls.
+  for (int idx = decls.size() - 1; idx >= 0; --idx) {
+    if (*decl == *decls[idx])
+      return decls[idx];
+  }
+
+  return nullptr;
+}
