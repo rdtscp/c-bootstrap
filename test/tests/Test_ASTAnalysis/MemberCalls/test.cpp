287dbbba6ebad3db9fa0fbd72fac734aa9b01ea0
Alexander Wilson <acwilson96@gmail.com>
2019-05-14 23:17:43 +0100

    Temp, update test.

--- test/tests/Test_ASTAnalysis/MemberCalls/test.cpp
+++ test/tests/Test_ASTAnalysis/MemberCalls/test.cpp
@@ -3,18 +3,20 @@ public:
   int b_func() { return 1; }
 };
 
-class Derived {
-public:
-  char m_int;
+// class Derived {
+// public:
+//   char m_int;
 
-  Base d_func() {
-    Base b;
-    const int unused = this->m_int;
-    return b;
-  }
-};
+//   Base d_func() {
+//     Base b;
+//     const int unused = this->m_int;
+//     return b;
+//   }
+// };
 
 int main() {
-  Derived myDerived;
-  int m_int = myDerived.d_func().b_func();
+  Base myBase;
+  int b_int = myBase.b_func();
+  // Derived myDerived;
+  // int m_int = myDerived.d_func().b_func();
 }
