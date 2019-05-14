5a6f6aaaa06e8e99402069b26a13f6af36e1ff4b
Alexander Wilson <acwilson96@gmail.com>
2019-05-14 01:44:30 +0100

    Remove dead code.

--- include/ast/Scope.h
+++ include/ast/Scope.h
@@ -35,21 +35,5 @@ public:
   virtual atl::shared_ptr<VarDecl>
   findVarDeclLocal(const atl::shared_ptr<Identifier> identifier,
                    const atl::shared_ptr<Decl> exemptDecl = nullptr) const = 0;
-
-  /* Old */
-
-  // atl::shared_ptr<ClassTypeDecl>
-  // resolveClassType(const atl::shared_ptr<ClassType> &type) const;
-
-  // atl::shared_ptr<VarDecl>
-  // resolveVarExpr(const atl::shared_ptr<Identifier> identifier) const;
-  // atl::shared_ptr<FunDecl> resolveFunCall(const atl::string funSignature)
-  // const;
-
-  // atl::shared_ptr<Decl>
-  // duplicateDeclaration(const atl::shared_ptr<Decl> &decl) const;
-
-  // atl::shared_ptr<Decl>
-  // duplicateDeclarationLocal(const atl::shared_ptr<Decl> &decl) const;
 };
 } // namespace ACC
