bd7fef5e5f7d34e5bf835fc076478d889c322f6c
Alexander Wilson <acwilson96@gmail.com>
2019-05-23 01:15:33 +0100

    Make FunSignature a lib

---
+++ src/ast/FunSignature.cpp
@@ -0,0 +1,51 @@
+#include "ast/FunSignature.h"
+
+using namespace ACC;
+
+FunSignature::FunSignature(const atl::shared_ptr<Type> p_funReturnType,
+                           const atl::shared_ptr<Identifier> p_funIdentifier,
+                           const atl::vector<atl::shared_ptr<Type>> p_funArgs,
+                           const atl::set<FunDecl::FunModifiers> p_funModifiers)
+    : funReturnType(p_funReturnType), funIdentifier(p_funIdentifier),
+      funArgs(p_funArgs), funModifiers(p_funModifiers) {}
+
+const unsigned int FunSignature::namespaceCount() const {
+  return funIdentifier->namespaceCount();
+}
+
+const atl::shared_ptr<Identifier> FunSignature::namespaceHead() const {
+  return funIdentifier->namespaceHead();
+}
+
+const FunSignature FunSignature::lowerNamespace() const {
+  return FunSignature(funReturnType, funIdentifier->namespaceTail(), funArgs,
+                      funModifiers);
+}
+
+bool FunSignature::operator==(const FunSignature &rhs) const {
+  // TODO: Incorporate return type?
+
+  if (funArgs.size() != rhs.funArgs.size())
+    return false;
+
+  // Check args match.
+  for (unsigned int idx = 0u; idx < funArgs.size(); ++idx)
+    if (*funArgs[idx] != *rhs.funArgs[idx])
+      return false;
+
+  // Check identifier.
+  if (*funIdentifier != *rhs.funIdentifier)
+    return false;
+
+  // Check Relevant Modifiers
+  const bool lhsConst = funModifiers.find(FunDecl::FunModifiers::CONST);
+  const bool rhsConst = rhs.funModifiers.find(FunDecl::FunModifiers::CONST);
+  if (lhsConst != rhsConst)
+    return false;
+
+  return true;
+}
+
+bool FunSignature::operator!=(const FunSignature &rhs) const {
+  return !(*this == rhs);
+}
