aa9d191960bc04b3d5bf4e29c3a28233386d0e97
Alexander Wilson <acwilson96@gmail.com>
2019-06-06 20:00:12 +0100

    opToStr function.

--- include/ast/Op.h
+++ include/ast/Op.h
@@ -19,4 +19,29 @@ enum class Op {
   ASSIGNADD
 };
 
+static atl::string opToStr(Op op) {
+  switch (op) {
+  case Op::ADD:
+    return  "+";
+  case Op::SUB:
+    return  "-";
+  case Op::GT:
+    return  ">";
+  case Op::LT:
+    return  "<";
+  case Op::GE:
+    return  ">=";
+  case Op::LE:
+    return  "<=";
+  case Op::EQ:
+    return  "==";
+  case Op::NE:
+    return  "!=";
+  case Op::ASSIGNADD:
+    return  "+=";
+  default:
+    return "TODO";
+  }
+} // namespace ACC
+
 } // namespace ACC
