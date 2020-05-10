5f51d6e7b848bdc0dd4bb7db4c368e222256a520
Alexander Wilson <acwilson96@gmail.com>
2020-05-10 04:15:04 +0100

    Add more unit test coverage for subscript op

--- test/tests/Test_CodeGeneration/SubscriptPtr/test.cpp
+++ test/tests/Test_CodeGeneration/SubscriptPtr/test.cpp
@@ -1,6 +1,10 @@
 void printf(const char *, const char *);
 void printf(const char *, const char);
 
+class Foo {
+public:
+  char *m_val;
+};
 
 int main(int argc, const char **argv) {
   char str[13] = "Hello World\n";
@@ -10,5 +14,10 @@ int main(int argc, const char **argv) {
   const int idx = 10;
 
   printf("str[10] has value: %c\n", str[idx]);
+
+  Foo f;
+  f.m_val = "Hello World\n";
+  printf("f.m_val[10] has value: %c\n", f.m_val[idx]);
+
   return 0;
 }
