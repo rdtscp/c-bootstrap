f28f2b4f9661bb08175eb676b2a43abbcadc651e
Alexander Wilson <acwilson96@gmail.com>
2020-05-10 03:42:47 +0100

    BinOp CodeGen Test

---
+++ test/tests/Test_CodeGeneration/BinOp/test.cpp
@@ -0,0 +1,29 @@
+void printf(const char *, const int);
+
+int main(int argc, const char **argv) {
+  int one = 1;
+  int two = 2;
+  int three = 3;
+  int four = 4;
+  
+  printf("one < two == %d\n", one < two);
+  printf("two < three == %d\n", two < three);
+  printf("three < one == %d\n", three < one);
+
+  printf("three > two == %d\n", three > two);
+  printf("two > one == %d\n", two > one);
+  printf("one > three == %d\n", one > three);
+
+  printf("two >= one == %d\n", two >= one);
+  printf("two >= two == %d\n", two >= two);
+  printf("two >= three == %d\n", two >= three);
+
+  printf("two <= one == %d\n", two <= one);
+  printf("two <= two == %d\n", two <= two);
+  printf("two <= three == %d\n", two <= three);
+m
+  printf("three % two == %d\n", three % two);
+  printf("four % two == %d\n", four % two);
+
+  return 0;
+}
