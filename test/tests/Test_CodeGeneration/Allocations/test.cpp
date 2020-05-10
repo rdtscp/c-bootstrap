436275f77c2f98c89c2316ae7572091acc9bc5cf
Alexander Wilson <acwilson96@gmail.com>
2020-05-10 02:37:16 +0100

    Nicer tests

--- test/tests/Test_CodeGeneration/Allocations/test.cpp
+++ test/tests/Test_CodeGeneration/Allocations/test.cpp
@@ -1,22 +1,25 @@
 void printf(const char *, const char *);
 void printf(const char *, const int);
+void printf(const char *);
 
 int main(int argc, const char **argv) {
   int * y = new int[1];
 
   *y = 1234;
-  printf("y has value: %d\n", *y);
+  printf("expected '1234'.\n");
+  printf("actual:  '%d'\n", *y);
 
   *y = 5678;
-  printf("y has value: %d\n", *y);
-
+  printf("expected '5678'.\n");
+  printf("actual:  '%d'\n", *y);
 
   char * x = new char[4];
   x[0] = 'a';
   x[1] = 'b';
   x[2] = 'c';
   x[3] = '\0';
-  printf("x has value: %s\n", x);
+  printf("expected 'abc'.\n");
+  printf("actual:  '%s'\n", x);
 
   return 0;
 }
