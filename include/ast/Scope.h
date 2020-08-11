44de4d84415052231d8d6b13ecdf241dc712fb15
Alexander Wilson <acwilson96@gmail.com>
2020-08-11 03:07:58 +0100

    Scope includes objects it should destruct on exit.

--- include/ast/Scope.h
+++ include/ast/Scope.h
@@ -10,6 +10,7 @@ class FunSignature;
 class Scope {
 public:
   atl::weak_ptr<Scope> outerScope;
+  atl::vector<atl::shared_ptr<VarDecl>> objectsToDestruct;
   atl::string scopeName;
 
   Scope() : outerScope(nullptr) {}
