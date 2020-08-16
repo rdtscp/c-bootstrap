9e798fd22a8d90552c710ae0662288098d0a122c
Alexander Wilson <acwilson96@gmail.com>
2020-08-16 17:34:01 +0100

    String test

--- test/tests/main.cpp
+++ test/tests/main.cpp
@@ -3,6 +3,6 @@
 int main(int argc, char **argv) {
   const char *raw = "World";
   test::string str(raw);
-  printf("Hello %s!\n", raw);
-  return 23;
+  printf("Hello %s!\n", str.c_str());
+  return 5u - str.size();
 }
