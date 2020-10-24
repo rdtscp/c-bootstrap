55ff8f9a743b7dfd7446c68363531a54d75dcfb6
Alexander Wilson <acwilson96@gmail.com>
2020-10-24 18:36:57 +0100

    More complex PassByValue Test.

--- test/tests/Test_CodeGeneration/PassByValue/test.cpp
+++ test/tests/Test_CodeGeneration/PassByValue/test.cpp
@@ -1,30 +1,70 @@
 
-void printf(const char *, const int);
+void printf(const char *, const char *);
 
-class bigint {
-public:
-  int value;
+typedef unsigned int uint;
 
-  bigint(const int v) : value(v) {}
+namespace test {
+void swap(uint &lhs, uint &rhs) {
+  uint temp = lhs;
+  lhs = rhs;
+  rhs = temp;
+}
+void swap(const char *&lhs, const char *&rhs) {
+  const char *temp = lhs;
+  lhs = rhs;
+  rhs = temp;
+}
+} // namespace test
 
-  ~bigint() {}
-};
-
-void use(bigint p) {
-  printf("p[%d]\n", p.value);
-
-  p.value = 10;
-  printf("p[%d]\n", p.value);
+uint char_buf_len(const char *buf) {
+  uint length = 0u;
+  while (*buf != '\0') {
+    ++length;
+    ++buf;
+  }
+  return length;
 }
 
+class string_view {
+private:
+  uint m_size;
+  const char *m_value;
+
+public:
+  string_view(const char *value)
+      : m_size(char_buf_len(value)), m_value(value) {}
+
+  string_view(const string_view &rhs)
+      : m_size(rhs.m_size), m_value(rhs.m_value) {}
+
+  string_view &operator=(string_view rhs) {
+    printf("operator=: '%s'\n", rhs.c_str());
+    test::swap(this->m_size, rhs.m_size);
+    test::swap(this->m_value, rhs.m_value);
+  }
+
+  ~string_view() {}
+
+  const char *c_str() { return this->m_value; }
+
+  const char *c_str() const { return this->m_value; }
+
+  uint size() const { return this->m_size; }
+};
+
+void print_copy(string_view copy) { printf("copy: '%s'\n", copy.c_str()); }
+
 int main(int argc, char **argv) {
-  bigint foo(3);
+  string_view test("Test");
+  printf("string_view test('Test'): '%s'\n", test.c_str());
 
-  printf("foo[%d]\n", foo.value);
+  string_view other("Other");
+  printf("string_view other('Other'): '%s'\n", other.c_str());
 
-  use(foo);
+  test = other;
+  printf("test = other: '%s'\n", test.c_str());
 
-  printf("foo[%d]\n", foo.value);
+  print_copy(test);
 
-  return foo.value - 3;
+  return 5u - char_buf_len(test.c_str());
 }
