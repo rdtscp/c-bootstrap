e8db5b204bee7a18f943455797b6378ddf797086
Alexander Wilson <acwilson96@gmail.com>
2020-10-24 19:11:52 +0100

    Update main to be cleaner

--- test/tests/main.cpp
+++ test/tests/main.cpp
@@ -1,8 +1,15 @@
 #include "test_string.h"
 
+void print(const test::string_view str) { printf("%s\n", str.c_str()); }
+
 int main(int argc, char **argv) {
-  const char *raw = "World";
-  test::string str(raw);
-  printf("Hello %s!\n", str.c_str());
-  return 5u - str.size();
+  const test::string_view hello_world("Hello World!");
+  print(hello_world);
+
+  {
+    const test::string_view copy = hello_world;
+    print(copy);
+  }
+
+  return hello_world.size() - 12u;
 }
